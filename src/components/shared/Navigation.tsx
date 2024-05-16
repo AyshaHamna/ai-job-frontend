ca6c5ab002251f047e5b80d650f1f76c369e16af
Aysha Hamna <ayshahamna57@gmail.com>
2024-05-16 22:15:53 +0530

    updated home navigation

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -11,7 +11,7 @@ function Navigation() {
         </Link>
       </div>
       <div className="flex justify-center gap-x-8 items-center">
-        <Link to={"/home"}>Home</Link>
+        <Link to={"/"}>Home</Link>
 
         <div className="flex gap-x-4 items-center">
           <SignedIn>
