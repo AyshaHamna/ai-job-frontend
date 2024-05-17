e692622c3f4c352c7610ab18bcd4f01c985e77ce
Aysha Hamna <ayshahamna57@gmail.com>
2024-05-17 17:06:45 +0530

    added menu icon to nav in sm size

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -1,31 +1,58 @@
 import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+
+const NavLinks = () => {
+  return (
+    <>
+      <div className="flex gap-x-4 items-center">
+        <Link to={"/"}>Home</Link>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
+        <SignedOut>
+          <Link to={"/sign-in"}>Sign In</Link>
+          <Button asChild>
+            <Link to={"/sign-up"}>Sign Up</Link>
+          </Button>
+        </SignedOut>
+      </div>
+    </>
+  );
+};
 
 function Navigation() {
-  return (
-    <nav className="flex py-12 justify-between items-center">
-      <div>
-        <Link to={"/"} className="text-4xl font-medium text-underlay-1">
-          HirelyAI
-        </Link>
-      </div>
-      <div className="flex justify-center gap-x-8 items-center">
-        <Link to={"/"}>Home</Link>
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
-        <div className="flex gap-x-4 items-center">
-          <SignedIn>
-            <UserButton afterSignOutUrl="/sign-in" />
-          </SignedIn>
-          <SignedOut>
-            <Link to={"/sign-in"}>Sign In</Link>
-            <Button asChild>
-              <Link to={"/sign-up"}>Sign Up</Link>
-            </Button>
-          </SignedOut>
+  return (
+    <>
+      <nav className="flex py-12 justify-between items-center">
+        <div>
+          <Link to={"/"} className="text-4xl font-medium text-underlay-1">
+            HirelyAI
+          </Link>
         </div>
-      </div>
-    </nav>
+
+        <div className="hidden sm:flex justify-center gap-x-8 items-center">
+          <NavLinks />
+        </div>
+
+        {/* menu icon */}
+        <div className="sm:hidden">
+          <button onClick={toggleNavbar}>{isOpen ? <X /> : <Menu />}</button>
+        </div>
+      </nav>
+      {isOpen && (
+        <div className="sm:hidden flex flex-col items-center pb-4 basis-full">
+          <NavLinks />
+        </div>
+      )}
+    </>
   );
 }
 
