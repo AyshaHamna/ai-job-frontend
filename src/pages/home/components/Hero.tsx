c39ffbf67cc25f6b2169ed5e8768dc594fd8f0c9
AyshaHamna <ayshahamna57@gmail.com>
2024-09-09 20:02:43 +0530

    home page UI updated

--- src/pages/home/components/Hero.tsx
+++ src/pages/home/components/Hero.tsx
@@ -1,8 +1,8 @@
 function Hero() {
   return (
     <section className="py-32 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero">
-      <div>
-        <h1 className="text-white text-center">Land your dream Job</h1>
+      <div className="relative z-10">
+        <h1 className="text-white text-center">Land your Dream Job</h1>
       </div>
     </section>
   );
